6975bfac21b0e777a7cb02de627ca5723922f3d3
devinvisible <devinvisible@gmail.com>
2016-12-01 23:21:24 -0500

    Initial checkin (for a simple proxy)

---
+++ coc-api/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+
+namespace coc_api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Proxy as a proof of concept starting point
+
+            var sw = Stopwatch.StartNew();
+
+            var proxy = new Proxy();
+            proxy.Start(new IPEndPoint(IPAddress.Any, 9339));
+
+            sw.Stop();
+
+            Console.WriteLine("Done({0}ms)! Listening on *:9339", sw.Elapsed.TotalMilliseconds);
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
